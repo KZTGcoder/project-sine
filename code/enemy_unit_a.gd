313780c9df482581c3b9e83c473dfdef2011aafb
KZTGcoder <kittipit787@gmail.com>
2025-11-13 19:06:49 +0700

    path following debug

--- code/enemy_unit_a.gd
+++ code/enemy_unit_a.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-
-@export var speed = 100
+@export var speed: float = 100.0
 
 func _process(delta: float) -> void:
-	get_parent().set_process(get_parent().get_process()+ speed*delta)
-	if get_parent().get_progress_ratio() == 1:
+	var path_follow_node = get_parent()
+	var new_progress = path_follow_node.get_progress() + speed * delta
+
+	path_follow_node.set_progress(new_progress)
+
+	if path_follow_node.get_progress_ratio() >= 1.0:
 		queue_free()
